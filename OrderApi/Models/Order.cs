469f2f2e277883563641df8e1f62174422c52296
Jonas Laursen <joamla96@gmail.com>
2019-03-08 12:22:25 +0100

    prepared order model for multiple products

--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace OrderApi.Models
 {
     public class Order
     {
         public int Id { get; set; }
         public DateTime? Date { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        public List<OrderLine> OrderLines { get; set; }
+		public int CustomerId { get; set; }
     }
 }
