276a01e67a91b04895101f1bd3b4e32a0d2b3761
Jonas Laursen <joamla96@gmail.com>
2019-03-08 12:56:58 +0100

    Fixed docker ports

--- OrderApi/Controllers/OrdersController.cs
+++ OrderApi/Controllers/OrdersController.cs
@@ -52,29 +52,29 @@ namespace OrderApi.Controllers
 
 
             // Call ProductApi to get the product ordered
-            RestClient c = new RestClient();
-            // You may need to change the port number in the BaseUrl below
-            // before you can run the request.
-            c.BaseUrl = new Uri("https://localhost:5001/api/products/");
-            var request = new RestRequest(order.ProductId.ToString(), Method.GET);
-            var response = c.Execute<Product>(request);
-            var orderedProduct = response.Data;
+            //RestClient c = new RestClient();
+            //// You may need to change the port number in the BaseUrl below
+            //// before you can run the request.
+            //c.BaseUrl = new Uri("https://localhost:5001/api/products/");
+            //var request = new RestRequest(order.ProductId.ToString(), Method.GET);
+            //var response = c.Execute<Product>(request);
+            //var orderedProduct = response.Data;
 
-            if (order.Quantity <= orderedProduct.ItemsInStock)
-            {
-                // reduce the number of items in stock for the ordered product,
-                // and create a new order.
-                orderedProduct.ItemsReserved += order.Quantity;
-                var updateRequest = new RestRequest(orderedProduct.Id.ToString(), Method.PUT);
-                updateRequest.AddJsonBody(orderedProduct);
-                var updateResponse = c.Execute(updateRequest);
+            //if (order.Quantity <= orderedProduct.ItemsInStock)
+            //{
+            //    // reduce the number of items in stock for the ordered product,
+            //    // and create a new order.
+            //    orderedProduct.ItemsReserved += order.Quantity;
+            //    var updateRequest = new RestRequest(orderedProduct.Id.ToString(), Method.PUT);
+            //    updateRequest.AddJsonBody(orderedProduct);
+            //    var updateResponse = c.Execute(updateRequest);
 
-                if (updateResponse.IsSuccessful)
-                {
-                    var newOrder = repository.Add(order);
-                    return CreatedAtRoute("GetOrder", new { id = newOrder.Id }, newOrder);
-                }
-            }
+            //    if (updateResponse.IsSuccessful)
+            //    {
+            //        var newOrder = repository.Add(order);
+            //        return CreatedAtRoute("GetOrder", new { id = newOrder.Id }, newOrder);
+            //    }
+            //}
 
             // If the order could not be created, "return no content".
             return NoContent();
--- OrderApi/Models/OrderLine.cs
+++ OrderApi/Models/OrderLine.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace OrderApi.Models {
 	public class OrderLine {
+		public int Id { get; set; }
 		public int ProductId { get; set; }
 		public int Quantity { get; set; }
 	}
