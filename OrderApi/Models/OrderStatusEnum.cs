030fd243b8cebb9468a4f40764e7bfe73e84e542
Jonas Laursen <joamla96@gmail.com>
2019-03-18 13:16:59 +0100

    created an order status thingy

---
+++ OrderApi/Models/OrderStatusEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models {
+	public enum OrderStatus {
+		completed, cancelled, shipped, paid
+	}
+}
