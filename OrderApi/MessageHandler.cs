1d4320c832574e51fffbe3ba480935f43e6f3764
Jonas Laursen <joamla96@gmail.com>
2019-03-18 13:25:18 +0100

    Created a message handler class

--- OrderApi/MessageHandler.cs
+++ OrderApi/MessageHandler.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using EasyNetQ
+using EasyNetQ;
+using Microsoft.Extensions.Configuration;
 
 namespace OrderApi {
-	public class MessageHandler {
-		private IBus bus;
+	public class MessageHandler : IDisposable {
+		public IBus bus { get; private set; }
+
+		public MessageHandler(IConfiguration config) {
+			var rabbitConfig = config.GetSection("RabbitConnection");
+			var connectionString = String.Format("host={0};virtualHost={1};username={2};password={3}", rabbitConfig["Host"], rabbitConfig["VirtualHost"], rabbitConfig["Username"], rabbitConfig["Password"]);
+			bus = RabbitHutch.CreateBus(connectionString);
+		}
+
+		public void Dispose() {
+			bus.Dispose();
+		}
 	}
 }
