cddb23da4147368f347862adeffdca1965db684d
Jonas Laursen <jona8690@edu.eal.dk>
2019-03-19 20:47:04 +0100

    prepared product api for message listening

---
+++ ProductApi/Infrastructure/MessageListener.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Data;
+using SharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Infrastructure {
+	public class MessageListener {
+		private string connectionString;
+		private ProductRepository customerRepository;
+
+		public MessageListener(IServiceProvider provider) {
+			using (var scope = provider.CreateScope()) {
+				var services = scope.ServiceProvider;
+				this.customerRepository = services.GetService<ProductRepository>();
+
+				var config = services.GetService<IConfiguration>();
+				this.connectionString = config.GetConnectionString("Rabbit");
+			}
+		}
+
+		private ProductInStockResponse ProductInStock(ProductInStockRequest request) {
+			var response = new ProductInStockResponse();
+
+			return response;
+		}
+	}
+}
