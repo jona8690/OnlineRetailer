6101fbf05e91b81b235edf12893a94c0f1d91435
Jonas Laursen <jona8690@edu.eal.dk>
2019-03-19 20:55:19 +0100

    respond

--- ProductApi/Infrastructure/MessageListener.cs
+++ ProductApi/Infrastructure/MessageListener.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProductApi.Data;
+using ProductApi.Models;
 using SharedModels;
 using System;
 using System.Collections.Generic;
@@ -12,12 +13,12 @@ using System.Threading.Tasks;
 namespace ProductApi.Infrastructure {
 	public class MessageListener {
 		private string connectionString;
-		private ProductRepository customerRepository;
+		private IRepository<Product> productRepository;
 
 		public MessageListener(IServiceProvider provider) {
 			using (var scope = provider.CreateScope()) {
 				var services = scope.ServiceProvider;
-				this.customerRepository = services.GetService<ProductRepository>();
+				this.productRepository = services.GetService<ProductRepository>();
 
 				var config = services.GetService<IConfiguration>();
 				this.connectionString = config.GetConnectionString("Rabbit");
@@ -36,6 +37,11 @@ namespace ProductApi.Infrastructure {
 
 		private ProductInStockResponse ProductInStock(ProductInStockRequest request) {
 			var response = new ProductInStockResponse();
+
+			var product = productRepository.Get(request.ProductId);
+			if (product.ItemsInStock >= request.Quantity)
+				response.Instock = true;
+			else response.Instock = false;
 
 			return response;
 		}
