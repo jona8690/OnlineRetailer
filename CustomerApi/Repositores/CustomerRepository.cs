1c9d45aabf0d79273ed6a6f64371477c20fa6efa
Yuki <carl7755@easv365.dk>
2019-03-21 14:28:49 +0100

    added bool method to retrieve the good standing

--- CustomerApi/Repositores/CustomerRepository.cs
+++ CustomerApi/Repositores/CustomerRepository.cs
@@ -1,41 +1,60 @@
-﻿using CustomerApi.Database;
-using CustomerApi.Models;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using CustomerApi.Database;
+using CustomerApi.Models;
 
-namespace CustomerApi.Repositores {
-	public class CustomerRepository {
-		private readonly CustomerApiContext db;
+namespace CustomerApi.Repositores
+{
+    public class CustomerRepository
+    {
+        private readonly CustomerApiContext db;
 
-		public CustomerRepository(CustomerApiContext context) {
-			db = context;
-		}
+        public CustomerRepository(CustomerApiContext context)
+        {
+            db = context;
+        }
 
-		public Customer Get(int CustomerId) {
-			return db.Customers.FirstOrDefault(x => x.customerId == CustomerId);
-		}
+        public Customer Get(int CustomerId)
+        {
+            return db.Customers.FirstOrDefault(x => x.customerId == CustomerId);
+        }
 
-		public IEnumerable<Customer> GetAll() {
-			return db.Customers.ToList();
-		}
+        public IEnumerable<Customer> GetAll()
+        {
+            return db.Customers.ToList();
+        }
 
-		public void Add(Customer item) {
-			db.Customers.Add(item);
-		}
+        public void Add(Customer item)
+        {
+            db.Customers.Add(item);
+        }
 
-		public void Update(Customer item) {
-			db.Customers.Update(item);
-		}
+        public void Update(Customer item)
+        {
+            db.Customers.Update(item);
+        }
 
-		public void Delete(int CustomerId) {
-			var item = this.Get(CustomerId);
-			db.Customers.Remove(item);
-		}
+        public void Delete(int CustomerId)
+        {
+            var item = this.Get(CustomerId);
+            db.Customers.Remove(item);
+        }
 
-		public bool Exists(int CustomerId) {
-			return db.Customers.Any(x => x.customerId == CustomerId);
-		}
-	}
+        public bool Exists(int CustomerId)
+        {
+            return db.Customers.Any(x => x.customerId == CustomerId);
+        }
+
+        public bool GoodStanding(int CustomerId)
+        {
+            var customer = db.Customers.FirstOrDefault(x => x.customerId == CustomerId);
+            if (customer != null && customer.creditStanding == creditStanding.Good)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
 }
